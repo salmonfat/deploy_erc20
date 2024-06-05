426437a6b73cdc5fad2d501fd163fbf07b191772
Joy <joy.lin@pstarc.com>
2024-06-05 10:54:39 +0800

    beacon proxy test

--- Tokens/test/BeaconProxy.t.sol
+++ Tokens/test/BeaconProxy.t.sol
@@ -10,7 +10,7 @@ import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 /// @custom:oz-upgrades-from src/ERC20ExampleV1.sol:ERC20ExampleV1
 contract ERC20ExampleV2 is ERC20ExampleV1{
-    function initializeV2() public initializer {
+    function initializeV2() public reinitializer(2) {
         version = "v0.0.2";
     }
 
@@ -36,13 +36,42 @@ contract BeaconPrpxyTest is BeaconProxyScript, Test {
         assertEq(client2_v1.version(), "v0.0.1");
     }
 
-    function testBeaconProxyUpdate() public {
+    function testBeaconProxyUpgrade() public {
         ERC20ExampleV2 newExample = new ERC20ExampleV2();
         vm.prank(sender);
         upgradeableBeacon.upgradeTo(address(newExample));
-
+        
         client1_v2 = ERC20ExampleV2(address(beaconProxy));
         client2_v2 = ERC20ExampleV2(address(beaconProxy2));
+
+        client1_v2.initializeV2();
+        client2_v2.initializeV2();
+        assertEq(client1_v2.version(), "v0.0.2");
+        assertEq(client2_v2.version(), "v0.0.2");
         assertEq(client1_v2.hello(), "hello");
+    }
+
+    function testUpgradeFail() public {
+        ERC20ExampleV2 newExample = new ERC20ExampleV2();
+        address notOwner = makeAddr("notOwner");
+        // test notOwner call upgrade
+        vm.startPrank(notOwner);
+        vm.expectRevert(abi.encodeWithSelector(bytes4(keccak256("OwnableUnauthorizedAccount(address)")), notOwner));
+        upgradeableBeacon.upgradeTo(address(newExample));
+        vm.stopPrank();
+    }
+
+    function testInitializeFail() public {
+        ERC20ExampleV2 newExample = new ERC20ExampleV2();
+        vm.prank(sender);
+        upgradeableBeacon.upgradeTo(address(newExample));
+        client1_v2 = ERC20ExampleV2(address(beaconProxy));
+        client2_v2 = ERC20ExampleV2(address(beaconProxy2));
+
+        // can not initialize twice
+        client1_v2.initializeV2();
+
+        vm.expectRevert(abi.encodeWithSelector(bytes4(keccak256("InvalidInitialization()"))));
+        client1_v2.initializeV2();
     }
 }
